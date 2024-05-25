514712a0741cce6b4ddf8c680d990a02b437eabb
Moshe Swed <mswed@beapot.com>
2024-05-25 16:36:50 -0400

    4 facts about fav number

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
+const UL = document.querySelector('ul')
 const BASEURL = ' http://numbersapi.com';
 const favNum = 13;
 const MIN = 7;
 const MAX = 12;
 
-// My fav number
-let trivia = axios.get(`${BASEURL}/${favNum}`, {params: {json: true}})
-// let trivia = axios.get(BASEURL, {params: {number: 13}})
+function getFavNumberFact() {
+    let trivia = axios.get(`${BASEURL}/${favNum}`, {params: {json: true}})
+    return trivia
 
-trivia.then(res => console.log(res))
+    let fact = ''
+    trivia
+        .then(res => {
+            fact = res
+            console.log('resolved fact', fact)
+        })
+        .catch(err => {
+            console.log(err)
+            return 'Error'
+        })
+    return fact
+}
+
+// My fav number
+getFavNumberFact()
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
 
 // Multiple numbers in a single query
 let multipleTrivia = axios.get(`${BASEURL}/${MIN}..${MAX}`, {params: {json: true}})
 // let trivia = axios.get(BASEURL, {params: {number: 13}})
 
 multipleTrivia.then(res => console.log(res))
+
+// Multiple facts about my fav number
+for (let i = 0; i < 5; i++) {
+    const fact = getFavNumberFact()
+        .then(res => {
+            const fact = document.createElement('li');
+            fact.innerText = res.data.text;
+            UL.append(fact)
+
+        })
+        .catch(err => console.log(err))
+}
