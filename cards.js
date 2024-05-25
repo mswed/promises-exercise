605ffc41eac902ed5ceedfc25f2d90c50a128e64
Moshe Swed <mswed@beapot.com>
2024-05-25 17:20:34 -0400

    create a new deck and draw two cards

--- cards.js
+++ cards.js
@@ -1,10 +1,10 @@
 const baseURL = 'https://deckofcardsapi.com/api/deck'
 
-function newDeck () {
+function newDeck() {
     return axios.get(`${baseURL}/new/shuffle/`, {params: {deck_count: 1}})
 }
 
-function drawCard (deck) {
+function drawCard(deck) {
     return axios.get(`${baseURL}/${deck}/draw`, {params: {count: 1}})
 }
 
@@ -14,11 +14,34 @@ newDeck()
         console.log('Created deck:', res)
         drawCard(res.data.deck_id)
             .then(res => {
-                console.log('Drew card:')
+                console.log(`Drew a card from deck ${res.data.deck_id}`)
                 console.log(res.data.cards[0].value)
                 console.log(res.data.cards[0].suit)
 
             }).catch(err => console.log(err))
+
+    })
+    .catch(err => console.log(err))
+
+// Create a new deck and draw two cards (one after the other)
+const cards = []
+newDeck()
+    .then(res => {
+        console.log('Created deck:', res)
+        drawCard(res.data.deck_id)
+            .then(res => {
+                console.log(`Drew a card from deck ${res.data.deck_id}`)
+                cards.push({value: res.data.cards[0].value, suit: res.data.cards[0].suit})
+                return drawCard(res.data.deck_id)
+                    .then(res => {
+                        console.log(`Drew a card from deck ${res.data.deck_id}`)
+                        cards.push({value: res.data.cards[0].value, suit: res.data.cards[0].suit})
+                        console.log(cards)
+
+                    })
+
+            })
+            .catch(err => console.log(err))
 
     })
     .catch(err => console.log(err))
