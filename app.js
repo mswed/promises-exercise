0dd8090b078e363d5ef711415571e6187a2fd324
Moshe Swed <mswed@beapot.com>
2024-05-25 16:16:48 -0400

    multiple numbers in one request

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const BASEURL = ' http://numbersapi.com';
 const favNum = 13;
+const MIN = 7;
+const MAX = 12;
 
+// My fav number
 let trivia = axios.get(`${BASEURL}/${favNum}`, {params: {json: true}})
 // let trivia = axios.get(BASEURL, {params: {number: 13}})
 
 trivia.then(res => console.log(res))
+
+// Multiple numbers in a single query
+let multipleTrivia = axios.get(`${BASEURL}/${MIN}..${MAX}`, {params: {json: true}})
+// let trivia = axios.get(BASEURL, {params: {number: 13}})
+
+multipleTrivia.then(res => console.log(res))
