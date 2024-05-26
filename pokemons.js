6f2c0d4452ddf90cd8105f13ba5cbf3bcf6fa367
Moshe Swed <mswed@beapot.com>
2024-05-25 22:46:39 -0400

    get flavor_text_entry in english

--- pokemons.js
+++ pokemons.js
@@ -1,24 +1,32 @@
-const baseURL = 'https://pokeapi.co/api/v2/ability/'
+const baseURL = 'https://pokeapi.co/api/v2/pokemon'
 
 function getAll() {
-    return axios.get(`${baseURL}`, {params: {limit: 367}})
+    return axios.get(`${baseURL}`, {params: {limit: 1302}})
+    // return axios.get(`${baseURL}`)
 }
 
 const selectedPokemons = []
 
 getAll()
     .then(res => {
-        console.log(res)
+        // console.log(res)
         for (let i = 0; i < 3; i++) {
-            const selectedPokemon = res.data.results[Math.floor(Math.random() * 367)]
-            console.log('Selected', selectedPokemon.url)
+            const selectedPokemon = res.data.results[Math.floor(Math.random() * 1302)]
             selectedPokemons.push(axios.get(selectedPokemon.url))
         }
         Promise.all(selectedPokemons)
             .then(pokemons => {
-                console.log('Hello?')
                 for (poke of pokemons) {
-                    console.log(poke.data)
+                    const name = poke.data.name
+                    const species = axios.get(`https://pokeapi.co/api/v2/pokemon-species/${poke.data.species.name}/`)
+                        .then(res => {
+                            for (fte of res.data.flavor_text_entries) {
+                                if (fte.language.name === 'en') {
+                                    console.log(`${name}: ${fte.flavor_text}`)
+                                    break
+                                }
+                            }
+                        })
                 }
             })
             .catch(err => console.log(err))
