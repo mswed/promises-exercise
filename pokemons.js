f06e799f9a8dbf0651a2aae570b641fb2e0c8faf
Moshe Swed <mswed@beapot.com>
2024-05-25 21:50:00 -0400

    select 3 random pokemons

--- pokemons.js
+++ pokemons.js
@@ -4,4 +4,24 @@ function getAll() {
     return axios.get(`${baseURL}`, {params: {limit: 367}})
 }
 
-getAll().then(res => console.log(res))
+const selectedPokemons = []
+
+getAll()
+    .then(res => {
+        console.log(res)
+        for (let i = 0; i < 3; i++) {
+            const selectedPokemon = res.data.results[Math.floor(Math.random() * 367)]
+            console.log('Selected', selectedPokemon.url)
+            selectedPokemons.push(axios.get(selectedPokemon.url))
+        }
+        Promise.all(selectedPokemons)
+            .then(pokemons => {
+                console.log('Hello?')
+                for (poke of pokemons) {
+                    console.log(poke.data)
+                }
+            })
+            .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+
